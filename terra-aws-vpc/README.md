8af4c929b23fa5df61ee9a2b5147cdf5446ce1b8
Tinhnx <nhoccon2111@gmail.com>
2022-06-07 19:21:16 +0700

    fix

---
+++ terra-aws-vpc/README.md
@@ -0,0 +1,1 @@
+![Structure](diagram.jpg)
--- terra-aws-vpc/module/vpc/main.tf
+++ terra-aws-vpc/module/vpc/main.tf
@@ -3,7 +3,7 @@ resource "aws_vpc" "vpc" {
   enable_dns_hostnames = true
 
   tags = {
-    "Name" = "custom"
+    "Name" = "terraform-vpc-custom"
   }
 }
 
